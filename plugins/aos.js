8c7f727eab6f3be687be7fde09a5cdd176673782
MuhammadM1998 <MuhammadM.1097@gmail.com>
2022-09-15 21:29:30 +0200

    Updated package.json

--- plugins/aos.js
+++ plugins/aos.js
@@ -4,7 +4,6 @@ import 'aos/dist/aos.css';
 export default defineNuxtPlugin((nuxtApp) => {
   if (typeof window !== 'undefined') {
     const aosOptions = {
-      disable: window.innerWidth < 640,
       duration: 600,
       easing: 'ease-in-out-cubic',
       once: true,
